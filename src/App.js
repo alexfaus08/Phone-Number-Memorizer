0c0c3f84d9ccc2541c29865e7af770fe56f48e90
Alexander Faus <alexander.faus@agilesof.com>
2021-12-28 18:42:46 -0500

    basic home page setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import PhoneNumberInput from './components/PhoneNumberInput';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { CssBaseline } from '@mui/material';
+import { Container } from '@mui/material';
+
 
 function App() {
   return (
-    <div className="App">
-        <div class="container">
-          <h2 class="">
-              Enter a Phone Number
-          </h2> 
+    <div>
+      <CssBaseline />
+      <Container maxWidth='md'>
+        <Box 
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}>
+          <Typography variant="h2" component="div" gutterBottom>
+            Enter a Phone Number
+          </Typography>
           <PhoneNumberInput />
-        </div>
+        </Box>
+      </Container>
     </div>
   );
 }
