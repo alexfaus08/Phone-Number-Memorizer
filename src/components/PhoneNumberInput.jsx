40c6c5305a238a54f0f01254505def2e2a046167
Alexander Faus <alexander.faus@agilesof.com>
2021-12-28 18:57:32 -0500

    phone number text field along with button
    non-functional

--- src/components/PhoneNumberInput.jsx
+++ src/components/PhoneNumberInput.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Box, TextField } from '@mui/material';
+import { Button } from '@mui/material';
 
 export const PhoneNumberInput = () => {
     return (
         <div>
-            <input class="" />
+            <Box sx={{ 
+                marginTop: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}>
+                <TextField label='Phone Number' variant='outlined' fullWidth placeholder='123-456-7890'/>
+                <Button variant="outlined" sx={{
+                    marginTop: 2,
+                }}>Start Memorizing!</Button>
+            </Box>
         </div>
     )
 }
