a1ce9b2e7a92a275fedfc7cab40d43d98364e83d
Alexander Faus <alexander.faus@agilesof.com>
2021-12-28 19:45:05 -0500

    added route to memorizing page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import logo from './logo.svg';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import './App.css';
-import PhoneNumberInput from './components/PhoneNumberInput';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import { Container } from '@mui/material';
-import { BrowserRouter } from "react-router-dom";
+import Home from './routes/Home';
+import MemorizeNumber from './routes/MemorizeNumber';
 
 
 function App() {
   return (
-    <div>
-      <Container maxWidth='md'>
-        <Box 
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}>
-          <Typography variant="h2" component="div" gutterBottom>
-            Enter a Phone Number
-          </Typography>
-          <PhoneNumberInput />
-        </Box>
-      </Container>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/memorize" element={<MemorizeNumber />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
--- src/components/PhoneNumberInput.jsx
+++ src/components/PhoneNumberInput.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import { Box, TextField } from '@mui/material';
 import { Button } from '@mui/material';
+import { useNavigate } from "react-router-dom";
 
 export const PhoneNumberInput = () => {
     const [number, setNumber] = useState('');
-
+    let navigate = useNavigate();
     return (
         <div>
             <Box sx={{ 
@@ -16,8 +17,11 @@ export const PhoneNumberInput = () => {
                 <TextField label='Phone Number' variant='outlined' fullWidth placeholder='123-456-7890' value={number} onInput={ e => setNumber(e.target.value)} />
                 <Button variant="outlined" sx={{
                     marginTop: 2,
-                }}
-                >Start Memorizing!
+                }}   
+                onClick={() => {
+                    navigate('/memorize')
+                  }}>
+                Start Memorizing!
                 </Button>
             </Box>
             { number }
