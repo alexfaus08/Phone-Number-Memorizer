1cc38922f3d3fd410fdb41b481084bc067102e8a
Alexander Faus <alexander.faus@agilesof.com>
2021-12-29 01:12:14 -0500

    fixed lint errors in PhoneNumberInput

--- src/components/PhoneNumberInput.jsx
+++ src/components/PhoneNumberInput.jsx
@@ -1,40 +1,38 @@
 import React from 'react'
-import { Box, Input } from '@mui/material';
-import { Button } from '@mui/material';
-import { useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from 'react-redux';
-import { setPhoneNumber } from '../redux/phoneNumberSlice';
+import { Box, Input, Button } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { setPhoneNumber } from '../redux/phoneNumberSlice'
 
 export const PhoneNumberInput = () => {
-    const num = useSelector(state => state.phoneNumber.value)
-    let navigate = useNavigate();
-    const dispatch = useDispatch();
+  const num = useSelector(state => state.phoneNumber.value)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-    const handleNumberChange = (event) => {
-        dispatch(setPhoneNumber(event.target.value));
-    }
+  const handleNumberChange = (event) => {
+    dispatch(setPhoneNumber(event.target.value))
+  }
 
-    return (
+  return (
         <div>
-            <Box sx={{ 
-                marginTop: 2,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
+            <Box sx={{
+              marginTop: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center'
             }}>
                 <Input label='Phone Number' variant='outlined' fullWidth placeholder='123-456-7890' value={ num } onChange={handleNumberChange} />
                 <Button variant="outlined" sx={{
-                    marginTop: 2,
-                }}   
+                  marginTop: 2
+                }}
                 onClick={() => {
-                    navigate('/memorize')
-                  }}>
+                  navigate('/memorize')
+                }}>
                 Start Memorizing!
                 </Button>
             </Box>
         </div>
-    )
+  )
 }
 
-
-export default PhoneNumberInput;
+export default PhoneNumberInput
