f216f051674b0ecae3171fc11a48e2e558c3af4b
Alexander Faus <alexander.faus@agilesof.com>
2021-12-28 19:53:21 -0500

    reconfigured router setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
 import './App.css';
-import Home from './routes/Home';
-import MemorizeNumber from './routes/MemorizeNumber';
+import { Outlet } from 'react-router-dom';
 
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/memorize" element={<MemorizeNumber />} />
-    </Routes>
-  </BrowserRouter>
+    <div>
+      <Outlet />
+    </div>
   );
 }
 
