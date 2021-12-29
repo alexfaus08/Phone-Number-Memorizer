d877b98269d1e8517b3e73daa68634de40f2c1cd
Alexander Faus <alexander.faus@agilesof.com>
2021-12-29 01:18:00 -0500

    fixed lint errors

--- src/redux/phoneNumberSlice.js
+++ src/redux/phoneNumberSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 export const phoneNumberSlice = createSlice({
   name: 'phoneNumber',
   initialState: {
-    value: '269-567-0819',
+    value: '269-567-0819'
   },
   reducers: {
     setPhoneNumber: (state, action) => {
--- src/routes/MemorizeNumber.jsx
+++ src/routes/MemorizeNumber.jsx
@@ -4,27 +4,27 @@ import { Box } from '@mui/system'
 import { TextField } from '@mui/material'
 
 const generateNumberPlaceholder = (number) => {
-    let placeholder = number.replaceAll('-', '\n');
-    return placeholder;
+  const placeholder = number.replaceAll('-', '\n')
+  return placeholder
 }
 
 const MemorizeNumber = () => {
-    const num = useSelector(state => state.phoneNumber.value)
-    const placeholder = generateNumberPlaceholder(num);
-    return (
+  const num = useSelector(state => state.phoneNumber.value)
+  const placeholder = generateNumberPlaceholder(num)
+  return (
         <div>
-            <Box sx={{ 
-                marginTop: 2,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
+            <Box sx={{
+              marginTop: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center'
             }}>
-            Time to memorize: { num } 
+            Time to memorize: { num }
             <br />
-            Type the number vertically: 
+            Type the number vertically:
             <TextField
               multiline
-              sx={{ textAlign: "center" }}
+              sx={{ textAlign: 'center' }}
               placeholder={placeholder}
               rows={3}
             />
