e9c9474067caa05d53dafbb82045279a86ae45f7
Alexander Faus <alexander.faus@agilesof.com>
2022-01-01 19:12:44 -0600

    started styling digit

--- src/components/Digit.jsx
+++ src/components/Digit.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { TextInput, StyleSheet } from 'react-native'
 
 const Digit = (props) => {
   return (
         <div className="digit">
-            { props.digit }
+            <TextInput placeholder={props.digit} style={styles.input} />
         </div>
   )
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10
+  }
+})
 
 export default Digit
