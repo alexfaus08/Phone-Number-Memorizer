ccd5d79ea48acb13c6e3510e015f9399ef5f9935
Alexander Faus <alexander.faus@agilesof.com>
2022-01-06 15:42:32 -0500

    added hint

--- src/components/PhoneNumberHint.jsx
+++ src/components/PhoneNumberHint.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, View } from 'react-native'
 
 const PhoneNumberHint = (props) => {
-  return <></>
+  const [isVisible, setIsVisible] = useState(false)
+
+  const handleClick = () => {
+    setIsVisible((isVisible) => (!isVisible))
+  }
+
+  return <div className="hint">
+    <View style={[{ width: '25%' }]}>
+      <Button
+          title={isVisible ? 'Hide Hint' : 'Show Hint'}
+          onPress={handleClick}
+      />
+      <div className="number-hint">
+        {
+          isVisible && <>
+            <div>
+              {props.phoneNumber.slice(0, 3)}
+            </div>
+            <div>
+              {props.phoneNumber.slice(3, 6)}
+            </div>
+            <div>
+              {props.phoneNumber.slice(6, 10)}
+            </div>
+          </>
+        }
+      </div>
+    </View>
+  </div>
 }
 
 export default PhoneNumberHint
