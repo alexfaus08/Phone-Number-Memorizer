b0b805751c260c3cf916ad2c3865ba998c279a00
Alexander Faus <alexander.faus@agilesof.com>
2022-01-06 12:39:11 -0500

    step 1 implemented

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Digit from './Digit'
 
 const Row = (props) => {
+  const [isLastRow, setIsLastRow] = useState(false)
+
+  useEffect(() => {
+    if (props.digits.length === 4) {
+      setIsLastRow(true)
+    }
+  }, [])
+
+  const hideMiddleDigit = (digits) => {
+    const temp = [...digits]
+    temp.forEach((digit) => {
+      digit.digit = '?'
+      digit.clear = true
+    })
+    temp[1] = {
+      editable: false,
+      digit: '-',
+      clear: true
+    }
+    if (isLastRow) {
+      temp[2] = {
+        editable: false,
+        digit: '-',
+        clear: true
+      }
+    }
+    return temp
+  }
+
+  const applyStep = (digits) => {
+    switch (props.step) {
+      case (1):
+        return hideMiddleDigit(digits)
+    }
+  }
+
   const renderDigits = () => {
-    const digitInputs = []
+    let digitInputs = []
     props.digits.forEach((digit) => {
       digitInputs.push({
         digit: digit.toString(),
@@ -11,6 +47,9 @@ const Row = (props) => {
         clear: true
       })
     })
+    if (props.step !== 0) {
+      digitInputs = applyStep(digitInputs)
+    }
     return digitInputs.map((digit, id) => {
       return <Digit digit={digit.digit} key={id} editable={digit.editable} clear={digit.clear} />
     })
