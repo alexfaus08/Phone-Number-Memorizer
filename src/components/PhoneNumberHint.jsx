3db965f2ed1aae8d50db8c0e490e7b0face970b6
Alexander Faus <alexander.faus@agilesof.com>
2022-01-06 19:55:52 -0500

    added collapse animation

--- src/components/PhoneNumberHint.jsx
+++ src/components/PhoneNumberHint.jsx
@@ -3,6 +3,7 @@ import Help from '@mui/icons-material/Help'
 import Chip from '@mui/material/Chip'
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
+import Collapse from '@mui/material/Collapse'
 
 const PhoneNumberHint = (props) => {
   const [isVisible, setIsVisible] = useState(false)
@@ -14,18 +15,20 @@ const PhoneNumberHint = (props) => {
           icon={<Help />}
           label="Hover for a hint..."
       />
-      <Box sx={{ visibility: isVisible ? 'visible' : 'hidden' }}>
-          <div className="number-hint">
-              <div>
-                  {props.phoneNumber.slice(0, 3)}
+      <Box>
+          <Collapse in={isVisible}>
+              <div className="number-hint">
+                  <div>
+                      {props.phoneNumber.slice(0, 3)}
+                  </div>
+                  <div>
+                      {props.phoneNumber.slice(3, 6)}
+                  </div>
+                  <div>
+                      {props.phoneNumber.slice(6, 10)}
+                  </div>
               </div>
-              <div>
-                  {props.phoneNumber.slice(3, 6)}
-              </div>
-              <div>
-                  {props.phoneNumber.slice(6, 10)}
-              </div>
-          </div>
+          </Collapse>
       </Box>
   </Stack>
 }
