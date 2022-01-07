eec81dc25f59e27e65b741b545f4ac88ba568d57
Alexander Faus <alexander.faus@agilesof.com>
2022-01-06 19:46:34 -0500

    hint layout and step tracker

--- src/components/PhoneNumberHint.jsx
+++ src/components/PhoneNumberHint.jsx
@@ -1,34 +1,33 @@
 import React, { useState } from 'react'
 import Help from '@mui/icons-material/Help'
 import Chip from '@mui/material/Chip'
+import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
 
 const PhoneNumberHint = (props) => {
   const [isVisible, setIsVisible] = useState(false)
 
-  return <div className="hint">
+  return <Stack>
+      <Box sx={{ visibility: isVisible ? 'visible' : 'hidden' }}>
+          <div className="number-hint">
+              <div>
+                  {props.phoneNumber.slice(0, 3)}
+              </div>
+              <div>
+                  {props.phoneNumber.slice(3, 6)}
+              </div>
+              <div>
+                  {props.phoneNumber.slice(6, 10)}
+              </div>
+          </div>
+      </Box>
       <Chip
           onMouseEnter={() => setIsVisible(true)}
           onMouseLeave={() => setIsVisible(false)}
           icon={<Help />}
           label="Hover for a hint..."
       />
-      <div className="number-hint">
-        {
-          isVisible && <>
-            <div>
-              {props.phoneNumber.slice(0, 3)}
-            </div>
-            <div>
-              {props.phoneNumber.slice(3, 6)}
-            </div>
-            <div>
-              {props.phoneNumber.slice(6, 10)}
-            </div>
-          </>
-        }
-      </div>
-
-  </div>
+  </Stack>
 }
 
 export default PhoneNumberHint
