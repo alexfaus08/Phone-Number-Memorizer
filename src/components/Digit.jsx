5350ad0d4f1615b64a1d72eab3ba4d229db5825a
Alexander Faus <alexander.faus@agilesof.com>
2022-01-14 15:44:51 -0500

    code cleanup

--- src/components/Digit.jsx
+++ src/components/Digit.jsx
@@ -5,27 +5,33 @@ import { addDigit } from '../features/validDigitsSlice'
 
 const Digit = (props) => {
   const dispatch = useDispatch()
-
+  const [isValid, setIsValid] = useState(false)
   const [value, setValue] = useState('')
+
+  const dispatchValidityChange = (validity) => {
+    dispatch(addDigit({ digitID: props.digitID, rowID: props.rowID, value: validity }))
+    setIsValid(validity)
+  }
+
   useEffect(() => {
     setValue('')
     if (!props.editable) {
-      dispatch(addDigit({ digitID: props.digitID, rowID: props.rowID, value: true }))
+      dispatchValidityChange(true)
     }
   }, [props.step])
 
   const handleTextInputChange = event => {
     setValue(event.target.value)
     if (event.target.value === props.correctDigit) {
-      dispatch(addDigit({ digitID: props.digitID, rowID: props.rowID, value: true }))
+      dispatchValidityChange(true)
     } else {
-      dispatch(addDigit({ digitID: props.digitID, rowID: props.rowID, value: false }))
+      dispatchValidityChange(false)
     }
   }
 
   return (
         <>
-          <TextField className="digitInput" disabled={!props.editable} placeholder={props.digit} onChange={handleTextInputChange} value={value} autoComplete='off'
+          <TextField className="digitInput" error={!isValid} disabled={!props.editable} placeholder={props.digit} onChange={handleTextInputChange} value={value} autoComplete='off'
           inputProps={{
             maxLength: 1,
             style: { textAlign: 'center' }
