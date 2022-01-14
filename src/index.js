3562c6e39333e2234b96fe10434e2297d5725816
Alexander Faus <alexander.faus@agilesof.com>
2022-01-14 14:40:05 -0500

    basic redux setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import { render } from 'react-dom'
 import App from './App'
 import React from 'react'
+import store from './app/store'
+import { Provider } from 'react-redux'
 
 const rootElement = document.getElementById('root')
 render(
-  <App />,
-  rootElement
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    rootElement
 )
